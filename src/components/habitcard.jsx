828373abdf32c176b1e9e3b85b311ca68d34af92
Ayudh Sarkar <fkm4hrrpk8@privaterelay.appleid.com>
2024-04-16 12:17:53 +0000

    More sectons added

---
+++ src/components/habitcard.jsx
@@ -0,0 +1,14 @@
+export default function HabitCard({ imgurl, alttext, title, description }) {
+  return (
+    <div
+      className="habit-card d-flex flex-column
+    justify-content-start align-items-start"
+    >
+      <img src={imgurl} alt={alttext} />
+      <div className="d-flex flex-column gap-32">
+        <span className="title">{title}</span>
+        <p className="description">{description}</p>
+      </div>
+    </div>
+  );
+}
