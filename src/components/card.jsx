44a082348827094ed11d6b01b02d2d1cda12b6ac
Ayudh Sarkar <31191645+ayudh07@users.noreply.github.com>
2024-04-17 01:06:13 +0530

    card gapping fix

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -11,7 +11,7 @@ export default function Card({variant, title, icon, description, children, style
     }else{
         return (
             <div className="card" style={styles}>
-                <div className="card-bodys d-flex flex-column">
+                <div className="card-bodys d-flex flex-column gap-3">
                     <div className="card-headers d-flex align-items-center">
                         <img src={icon} alt={title} className="icon"/>
                         <span className="card-titles">{title}</span>
