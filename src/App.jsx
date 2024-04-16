27938bfbc0562dbbf7f5b06a5340b87c06cb0a6e
Ayudh Sarkar <31191645+ayudh07@users.noreply.github.com>
2024-04-16 22:52:48 +0530

    background added

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import race from './assets/images/icon-race.svg'
 function App() {
   return (
     <>
+      <div className="gradient-background"></div>
       <Section
         predefinedClasses="d-flex flex-column gap-32 section-height"
       >
